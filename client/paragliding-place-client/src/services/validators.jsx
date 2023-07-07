567b0b8e7357652fab868853f587d30c57c1ca80
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-07-07 11:09:59 +0300

    add css file for validatos and delete style from html

--- client/paragliding-place-client/src/services/validators.jsx
+++ client/paragliding-place-client/src/services/validators.jsx
@@ -1,9 +1,11 @@
+import "./validators.css"
+
 export const findEmptyValue = (value) => {
     if (value === 1) {
-        return <span style={{ fontSize: '15px', fontStyle: 'italic' }}>Not selected</span>
+        return <span className="findEmptyFieldContainer">Not selected</span>
     }
     if (value === "No") {
-        return <span style={{ fontSize: '15px', fontStyle: "italic" }}>Not selected</span>
+        return <span className="findEmptyFieldContainer">Not selected</span>
     }
     return value
 }
