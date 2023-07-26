ee8fb8aed271d2c2189277f961e284097bc8f2b5
A.Stoychev <109471350+AStoychev@users.noreply.github.com>
2023-07-26 11:21:18 +0300

    Modified delete some comments in constants

--- client/paragliding-place-client/src/constants/constants.jsx
+++ client/paragliding-place-client/src/constants/constants.jsx
@@ -1,12 +1,5 @@
-import { TbCircleLetterA, TbCircleLetterB, TbCircleLetterC, TbCircleLetterD } from "react-icons/tb";
-
 // This is constants for difficulty level of the places
 export const radioButtonDifficultyLevel = ["A", "B", "C", "D"];
-
-// export const checkButtonDirectionsEast = ["e", "es", "ees", "en", "een", "ene", "ese"];
-// export const checkButtonDirectionsWest = ["w", "ws", "wws", "wn", "wwn", "wnw", "wsw"];
-// export const checkButtonDirectionsNorth = ["n", "nw", "nnw", "ne", "nne", "nwn", "nen"];
-// export const checkButtonDirectionsSouth = ["s", "sw", "ssw", "se", "sse", "sws", "ses"];
 
 export const checkButtonDirections = {
     "e": ["e", "es", "ees", "en", "een", "ene", "ese"],
